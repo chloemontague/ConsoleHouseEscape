9cdc6500a3cb5fd5b2d509022aba34d218b69130
markinfante <minfante@vassar.edu>
2019-12-02 17:21:26 -0500

    Added beginning shells of each class.

---
+++ ConsoleHouseEscape.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class ConsoleHouseEscape {
+
+    public static void main (String[] args){
+        Console.PrintLine("Hello User. I have nothing for you to do yet.");
+    }
+}
---
+++ Dictionary.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Dictionary {
+
+    // the names of every location and their descriptions
+
+    String[] locations = {};
+
+    String[] locdescriptions = {};
+
+    // the names of every object and their descriptions
+
+    String[] objects = {};
+
+    String[] objdescriptions = {};
+
+    // miscellaneous statements
+
+    String[] misc = {};
+
+}
